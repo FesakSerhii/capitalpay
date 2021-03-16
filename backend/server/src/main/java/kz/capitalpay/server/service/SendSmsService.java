e3523760f50698dd722a609804f7ece5a09d6344
root <rnOOBaxbC1>
2021-03-16 10:09:46 +0200

    Test SMS

--- backend/server/src/main/java/kz/capitalpay/server/service/SendSmsService.java
+++ backend/server/src/main/java/kz/capitalpay/server/service/SendSmsService.java
@@ -49,7 +49,7 @@ public class SendSmsService {
             if (phone.contains("+77")) {
 
 //                String mes =  URLEncoder.encode(text, StandardCharsets.UTF_8.toString());
-                String mes =  text.replace(" ","+");
+                String mes =  text.replace("+","%2B").replace(" ","+");
                 logger.info(mes);
                 String url = smsSendUrl +
                         "?login=" + smsSendLogin +
