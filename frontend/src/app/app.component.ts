c90ae667d5f9f8c8fd962c651a8fadb39cae71c9
Front <mk@zyxit.com>
2021-03-28 01:16:12 +0200

    fixed adaptive 768&320

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,19 @@
 import {Component, HostListener, Inject} from '@angular/core';
 import {DOCUMENT} from '@angular/common';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [NgbCarouselConfig]
 })
 export class AppComponent {
   title = 'capitalPay';
+  constructor(config: NgbCarouselConfig) {
+    //
+    config.interval = 2000;
+    config.keyboard = true;
+    config.pauseOnHover = true;
+  }
 }
--- frontend/src/app/main-page/main-page.component.ts
+++ frontend/src/app/main-page/main-page.component.ts
@@ -1,19 +1,25 @@
 import {Component, HostListener, OnInit, ViewChild} from '@angular/core';
-import {NgbTabset} from "@ng-bootstrap/ng-bootstrap";
+import {NgbCarouselConfig, NgbTabset} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss'],
+  providers: [NgbCarouselConfig]
 })
 export class MainPageComponent implements OnInit {
   currentTabId = '0';
   width = 0;
+  activeAnimation = false;
   constructor() { }
   // @ViewChild("tab", {static: false}) tab: NgbTabset;
 
   ngOnInit(): void {
     this.width = document.body.clientWidth;
+    setInterval(() => {
+      this.activeAnimation = this.isInViewport(document.getElementById('pay'));
+    }, 500);
+
     // this.tabset.select('1');
   }
   nextTab(): void{
@@ -23,5 +29,14 @@ export class MainPageComponent implements OnInit {
   prevTab(): void{
     // this.tab.select(String(+this.currentTabId-1));
     this.currentTabId = String(+this.currentTabId - 1);
+  }
+  isInViewport(elem: any): boolean {
+    const bounding = elem.getBoundingClientRect();
+    return (
+      bounding.top >= 0 &&
+      bounding.left >= 0 &&
+      bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
   }
 }
