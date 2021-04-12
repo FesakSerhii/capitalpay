ad94da032c1ac847481a43a0f2a165925d8338f5
root <rnOOBaxbC1>
2021-04-12 09:37:29 +0300

    One Support Request

--- backend/server/src/main/java/kz/capitalpay/server/help/controller/SupportController.java
+++ backend/server/src/main/java/kz/capitalpay/server/help/controller/SupportController.java
@@ -20,7 +20,7 @@ import java.security.Principal;
 import static kz.capitalpay.server.login.service.ApplicationRoleService.*;
 
 @RestController
-@RequestMapping("/api/v1/support")
+@RequestMapping(value = "/api/v1/support", produces = "application/json;charset=UTF-8")
 public class SupportController {
 
     Logger logger = LoggerFactory.getLogger(SupportController.class);
