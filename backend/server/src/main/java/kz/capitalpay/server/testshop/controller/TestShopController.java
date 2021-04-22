9865ae3d35f3476f27459173b489e7c270a5ffe1
root <rnOOBaxbC1>
2021-04-22 06:23:46 +0300

    Test Shop Temp Page

--- backend/server/src/main/java/kz/capitalpay/server/testshop/controller/TestShopController.java
+++ backend/server/src/main/java/kz/capitalpay/server/testshop/controller/TestShopController.java
@@ -1,14 +1,15 @@
 package kz.capitalpay.server.testshop.controller;
 
 import com.google.gson.Gson;
+import kz.capitalpay.server.testshop.service.TestShopService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
-@RequestMapping(value = "/testshop", produces = "application/json;charset=UTF-8")
+@Controller(value = "/testshop")
 public class TestShopController {
 
     Logger logger = LoggerFactory.getLogger(TestShopController.class);
@@ -16,6 +17,14 @@ public class TestShopController {
     @Autowired
     Gson gson;
 
+    @Autowired
+    TestShopService testShopService;
 
+    @GetMapping("/testshop/temporary/page")
+    String showTemporaryPage(ModelMap modelMap) {
+        Long bilId = testShopService.getNewBillId();
+        modelMap.addAttribute("bilId", bilId);
+        return "testshoptemppage";
+    }
 
 }
---
+++ backend/server/src/main/java/kz/capitalpay/server/testshop/service/TestShopService.java
@@ -0,0 +1,14 @@
+package kz.capitalpay.server.testshop.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestShopService {
+
+    Long lastBillId = System.currentTimeMillis();
+
+    public Long getNewBillId(){
+        return lastBillId++;
+    }
+
+}
