7aeacb768ed1f07a59cb6b3e8c7deff4b7013591
root <rnOOBaxbC1>
2021-04-22 10:02:37 +0300

    PaySystem Button List

--- backend/server/src/main/java/kz/capitalpay/server/paysystems/controller/TemporaryPaysystemPageController.java
+++ backend/server/src/main/java/kz/capitalpay/server/paysystems/controller/TemporaryPaysystemPageController.java
@@ -1,13 +1,16 @@
 package kz.capitalpay.server.paysystems.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TemporaryPaysystemPageController {
 
     @GetMapping("/paysystems/temporary/page")
-    String showTemporaryPage(){
+    String showTemporaryPage(ModelMap modelMap, @RequestParam String paymentid){
+        modelMap.addAttribute("paymentId",paymentid);
         return "paysystemtemppage";
     }
 
