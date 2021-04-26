84b579b1713c7bda1517bb3f64bd40f2a1c8210c
root <rnOOBaxbC1>
2021-04-26 14:36:46 +0300

    Card Pay

--- backend/server/src/main/resources/static/testsystem/js/script.js
+++ backend/server/src/main/resources/static/testsystem/js/script.js
@@ -1,3 +1,13 @@
-$(".select .head").click(function () {
-    $(this).parent().toggleClass("opened");
+$(function () {
+    $(".select .head").click(function () {
+        $(this).parent().toggleClass("opened");
+    })
+
+    $(".select li").click(function () {
+        const $select = $(this).closest(".select");
+        const $input = $select.find("input");
+        $input.val(this.innerText);
+        $select.find(".head").text(this.innerText);
+        $select.removeClass("opened");
+    })
 })
