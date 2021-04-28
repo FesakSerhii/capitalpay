a0675c49fc438ff991a4a47a81b3ad41e5da7ffd
root <rnOOBaxbC1>
2021-04-28 16:27:05 +0300

    Add Bank

---
+++ backend/server/src/main/java/kz/capitalpay/server/paysystems/systems/halykbank/sevice/HalykService.java
@@ -0,0 +1,24 @@
+package kz.capitalpay.server.paysystems.systems.halykbank.sevice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HalykService {
+
+    Logger logger = LoggerFactory.getLogger(HalykService.class);
+
+
+    public String getPaymentButton() {
+
+        return " <form class=\"tpsform_radioitem\" method=\"post\" action=\"https://api.capitalpay.kz/testsystem/pay\">" +
+
+                "<button type=\"submit\">" +
+                "<span class=\"tpsform_radioico\"><img src=\"/paysystems/img/NBK_Logo.png\" alt=\"\"></span>" +
+                "<span class=\"tpsform_radioname\">Test payment system</span>" +
+                "</button>\n" +
+                "</form>";
+
+    }
+}
