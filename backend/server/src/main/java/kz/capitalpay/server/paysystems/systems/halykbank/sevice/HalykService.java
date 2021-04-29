22449795ce45570a442cc0af2338fd5d78473146
root <rnOOBaxbC1>
2021-04-29 08:52:59 +0300

    Register Halyk Payment

--- backend/server/src/main/java/kz/capitalpay/server/paysystems/systems/halykbank/sevice/HalykService.java
+++ backend/server/src/main/java/kz/capitalpay/server/paysystems/systems/halykbank/sevice/HalykService.java
@@ -71,7 +71,8 @@ public class HalykService {
         halykPayment.setStatus(payment.getStatus());
         halykPayment.setTotalAmount(payment.getTotalAmount());
 
-       halykPayment = halykPaymentRepository.save(halykPayment);
+       halykPayment = halykPaymentRepository.saveAndFlush(halykPayment);
+
 
         logger.info("Halyk Payment: ", gson.toJson(halykPayment));
         Long id = halykPayment.getId();
