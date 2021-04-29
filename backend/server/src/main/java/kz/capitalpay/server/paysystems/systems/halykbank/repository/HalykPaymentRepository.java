5ccaa2bb0c586dd17aff7e27f2d82b5f7289ad4e
root <rnOOBaxbC1>
2021-04-29 08:21:14 +0300

    Register Halyk Payment

---
+++ backend/server/src/main/java/kz/capitalpay/server/paysystems/systems/halykbank/repository/HalykPaymentRepository.java
@@ -0,0 +1,8 @@
+package kz.capitalpay.server.paysystems.systems.halykbank.repository;
+
+import kz.capitalpay.server.paysystems.systems.halykbank.model.HalykPayment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HalykPaymentRepository extends JpaRepository<HalykPayment,Long> {
+
+}
