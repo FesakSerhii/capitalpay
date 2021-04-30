5fa0bdeb78a733dbc87d4cd1baab01171cede627
root <rnOOBaxbC1>
2021-04-30 19:48:36 +0300

    Complited Payment URL encode

--- backend/server/src/main/java/kz/capitalpay/server/paysystems/systems/halykbank/kkbsign/KKBSign.java
+++ backend/server/src/main/java/kz/capitalpay/server/paysystems/systems/halykbank/kkbsign/KKBSign.java
@@ -184,6 +184,9 @@ public class KKBSign implements Serializable {
 
     private String value(final String s, final String str) {
         System.out.println("Value:" + str);
+
+        System.out.println(s.indexOf(str));
+        System.out.println(s.indexOf( 34,s.indexOf(str)));
         final int n = s.indexOf(34, s.indexOf(str)) + 1;
         System.out.println(s.substring(n, s.indexOf(34, n)));
         System.out.println("\n");
