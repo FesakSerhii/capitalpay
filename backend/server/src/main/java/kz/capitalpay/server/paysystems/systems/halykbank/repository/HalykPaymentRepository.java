fb77348b2e65bb2a3095d02f8a8633db49222edb
root <rnOOBaxbC1>
2021-04-30 14:03:50 +0300

    Parse Halyk Response

--- backend/server/src/main/java/kz/capitalpay/server/paysystems/systems/halykbank/repository/HalykPaymentRepository.java
+++ backend/server/src/main/java/kz/capitalpay/server/paysystems/systems/halykbank/repository/HalykPaymentRepository.java
@@ -4,5 +4,5 @@ import kz.capitalpay.server.paysystems.systems.halykbank.model.HalykPayment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HalykPaymentRepository extends JpaRepository<HalykPayment, Long> {
-    HalykPayment findTopByBillId(String billId);
+    HalykPayment findTopByHalykId(String halykId);
 }
