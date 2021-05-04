ebc6ade3f705c18725dd6c0393a604a2cd47f23d
root <rnOOBaxbC1>
2021-05-04 08:19:02 +0300

    Signed XML

--- backend/server/src/main/java/kz/capitalpay/server/paysystems/systems/halykbank/sevice/HalykService.java
+++ backend/server/src/main/java/kz/capitalpay/server/paysystems/systems/halykbank/sevice/HalykService.java
@@ -236,8 +236,8 @@ public class HalykService {
             logger.info("signedXML: {}", signedXML);
 
             Map<String, String> vars = new HashMap<>();
-            vars.put("signedXML", URLEncoder.encode(signedXML, Charset.defaultCharset()));
-            logger.info("Encode request: {}", sendOrderActionLink + "/jsp/remote/control.jsp?" + vars.get("signedXML"));
+            vars.put("signedXML", signedXML);
+//            logger.info("Encode request: {}", sendOrderActionLink + "/jsp/remote/control.jsp?" + vars.get("signedXML"));
             String response = restTemplate.getForObject(sendOrderActionLink + "/jsp/remote/control.jsp?{signedXML}",
                     String.class, vars);
             logger.info("response: {}", response);
