6ab2c84a4df89887226b34cb2c854720865a467f
root <rnOOBaxbC1>
2021-05-11 09:41:38 +0300

    Halyk 3DS listener

--- backend/server/src/main/java/kz/capitalpay/server/paysystems/controller/PublicPaysystemController.java
+++ backend/server/src/main/java/kz/capitalpay/server/paysystems/controller/PublicPaysystemController.java
@@ -8,13 +8,17 @@ import kz.capitalpay.server.paysystems.systems.halyksoap.service.HalykSoapServic
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.LinkedHashMap;
 
 @Controller
 @RequestMapping(value = "/public/paysystem", produces = "application/json;charset=UTF-8")
@@ -34,6 +38,9 @@ public class PublicPaysystemController {
 
     @Autowired
     PaymentService paymentService;
+
+    @Value("${halyk.soap.termurl}")
+    String TermUrl;
 
 //
 //    @PostMapping("/system/buttonlist")
@@ -74,11 +81,35 @@ public class PublicPaysystemController {
             httpResponse.setHeader("Location", "https://capitalpay.kz/");
             httpResponse.setStatus(302);
         }else{
-            httpResponse.setHeader("Location", result);
-            httpResponse.setStatus(302);
+            try {
+                LinkedHashMap<String, String> param = gson.fromJson(result, LinkedHashMap.class);
 
+                httpResponse.setHeader("Location", param.get("/public/paysystem/secure/redirect" +
+                        "?acsUrl="+param.get("acsUrl")+
+                        "&MD="+param.get("MD")+
+                        "&PaReq="+param.get("PaReq")));
+
+                httpResponse.setStatus(302);
+            }catch (Exception e){
+                e.printStackTrace();
+
+            }
         }
     }
 
+
+    @GetMapping("/secure/redirect")
+    String secureRedirect(ModelMap modelMap, @RequestParam String acsUrl,
+                          @RequestParam String MD,
+                          @RequestParam String PaReq){
+
+        modelMap.addAttribute("acsUrl",acsUrl);
+        modelMap.addAttribute("MD",MD);
+        modelMap.addAttribute("PaReq",PaReq);
+        modelMap.addAttribute("TermUrl",TermUrl);
+
+        return "paysystems/secureredirect";
+
+    }
 
 }
