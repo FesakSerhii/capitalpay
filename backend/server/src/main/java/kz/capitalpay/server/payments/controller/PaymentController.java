6c2622c354ca9bc119b94a9bc9b5e504bc0af607
root <rnOOBaxbC1>
2021-05-19 05:39:47 +0300

    One Payment

--- backend/server/src/main/java/kz/capitalpay/server/payments/controller/PaymentController.java
+++ backend/server/src/main/java/kz/capitalpay/server/payments/controller/PaymentController.java
@@ -7,13 +7,16 @@ import kz.capitalpay.server.payments.service.PaymentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.security.RolesAllowed;
+import javax.validation.Valid;
 import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
 
 import static kz.capitalpay.server.login.service.ApplicationRoleService.*;
 
@@ -39,10 +42,21 @@ public class PaymentController {
 
     @PostMapping("/one")
     @RolesAllowed({ADMIN, OPERATOR, MERCHANT})
-    ResultDTO onePayment(Principal principal, @RequestBody OnePaymentDetailsRequestDTO request) {
+    ResultDTO onePayment(Principal principal, @Valid @RequestBody OnePaymentDetailsRequestDTO request) {
         logger.info("One payment");
         return paymentService.onePayment(principal, request);
     }
 
-
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResultDTO handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResultDTO(false, errors, -2);
+    }
 }
--- backend/server/src/main/java/kz/capitalpay/server/payments/dto/OnePaymentDetailsRequestDTO.java
+++ backend/server/src/main/java/kz/capitalpay/server/payments/dto/OnePaymentDetailsRequestDTO.java
@@ -1,6 +1,9 @@
 package kz.capitalpay.server.payments.dto;
 
+import javax.validation.constraints.NotNull;
+
 public class OnePaymentDetailsRequestDTO {
+    @NotNull
     String guid;
 
     public String getGuid() {
