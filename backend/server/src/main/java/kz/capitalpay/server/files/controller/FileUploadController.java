05ccd01be8bf5fc12c6574783b0ea7002584b060
Roman <matviichuk.office@gmail.com>
2021-07-27 15:06:39 +0300

    added test data

--- backend/server/src/main/java/kz/capitalpay/server/files/controller/FileUploadController.java
+++ backend/server/src/main/java/kz/capitalpay/server/files/controller/FileUploadController.java
@@ -7,10 +7,7 @@ import kz.capitalpay.server.files.service.FileStorageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.security.Principal;
@@ -28,6 +25,7 @@ public class FileUploadController {
     FileStorageService fileStorageService;
 
     @PostMapping("/upload")
+    @CrossOrigin(origins = "https://api.capitalpay.kz")
     ResultDTO uploadFile(Principal principal, @RequestParam MultipartFile multipartFile) {
         logger.info(multipartFile.getOriginalFilename());
         return fileStorageService.uploadFile(multipartFile,principal);
