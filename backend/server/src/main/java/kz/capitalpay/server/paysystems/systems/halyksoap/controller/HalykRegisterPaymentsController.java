bedc9d7dd666b96c3b98b586a33d7b1dc6702773
Serhii <serhiipasalskyi@gmail.com>
2021-09-30 20:32:48 +0300

    Return payment register as blob

--- backend/server/src/main/java/kz/capitalpay/server/paysystems/systems/halyksoap/controller/HalykRegisterPaymentsController.java
+++ backend/server/src/main/java/kz/capitalpay/server/paysystems/systems/halyksoap/controller/HalykRegisterPaymentsController.java
@@ -2,6 +2,7 @@ package kz.capitalpay.server.paysystems.systems.halyksoap.controller;
 
 import kz.capitalpay.server.paysystems.systems.halyksoap.dto.RegisterPaymentsDateDTO;
 import kz.capitalpay.server.paysystems.systems.halyksoap.service.HalykRegisterPaymentsService;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.security.RolesAllowed;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 
 import static kz.capitalpay.server.login.service.ApplicationRoleService.ADMIN;
@@ -30,8 +33,7 @@ public class HalykRegisterPaymentsController {
     @Autowired
     private HalykRegisterPaymentsService halykRegisterPaymentsService;
 
-    @RequestMapping(value = "/download", method = RequestMethod.POST,
-            produces = "text/plain;charset=UTF-8")
+    @RequestMapping(value = "/download", method = RequestMethod.POST)
     @RolesAllowed({ADMIN, OPERATOR})
     public ResponseEntity<Object> halykRegisterPaymentsDownload(@RequestBody RegisterPaymentsDateDTO dateDTO)
             throws IOException {
@@ -40,7 +42,7 @@ public class HalykRegisterPaymentsController {
         byte[] bytes = Files.readAllBytes(file.toPath());
         ByteArrayResource byteResource = new ByteArrayResource(bytes);
         return ResponseEntity.ok()
-                .contentType(MediaType.TEXT_PLAIN)
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
                 .header(HttpHeaders.CONTENT_DISPOSITION, String.format("attachment; filename=\"%s\"", file.getName()))
                 .body(byteResource);
     }
