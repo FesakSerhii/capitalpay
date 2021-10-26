ec70144887b2053f58e2d243f6f9863e32eb3352
mikePavliuk <napilos@zyxit.com>
2021-10-26 14:59:44 +0300

    change api.component

--- frontend/src/app/merchant-panel/api/api.component.ts
+++ frontend/src/app/merchant-panel/api/api.component.ts
@@ -7,38 +7,46 @@ import {StaticPageService} from '../../../../projects/admin-panel/src/app/servic
   styleUrls: ['./api.component.scss']
 })
 export class ApiComponent implements OnInit {
-  apiTitles=[];
+  apiTitles=[
+    {title:'Валюты',id:0,sections:[],tags:['currency/system/list','currency/system/edit','currency/system/add','currency/merchant/list','currency/cashbox/list','currency/cashbox/edit']},
+    {title:'Платежные системы',id:1,sections:[],tags:['paysystem/system/list','paysystem/system/enable','paysystem/merchant/list','paysystem/merchant/edit','paysystem/cashbox/list','paysystem/cashbox/edit','интернет-магазин']},
+    {title:'Кассы',id:2,sections:[],tags:['cashbox/create','cashbox/changename','cashboxsetting/get','cashboxsetting/set','cashbox/delete','cashbox/all']},
+    {title:'Платежи мерчанта',id:3,sections:[],tags:['payments/list','payments/one','paysystems/halyk/get','paysystems/halyk/set','paysystems/register/download']}
+  ];
   tags=['currency','paysystem','cashbox']
   apiSections=[]
   container:any = null;
   activeSectionId:number = null;
+  activeTitleId:number = null;
   constructor(private staticPageService:StaticPageService) { }
 
   ngOnInit(): void {
-    const promises = []
-    for(let tag of this.tags){
-      const reqObj = {
-        "language": 'RUS',
-        "tag": tag
-      }
-      promises.push(this.staticPageService.getStaticPage(reqObj));
-    }
-    Promise.all(promises).then(resp=>{
-      this.apiSections = resp.map(el=>{
-        return el.data
-      })
-      this.apiTitles = resp.map(el=>{
-       return el.data.name
-      })
-    });
+    this.getSections()
   }
   openSection(item) {
-    console.log(this.activeSectionId !== item.id);
     if(document.getElementById('content').childElementCount){
       document.getElementById('content').innerHTML = ""
+      this.activeSectionId = null;
     }else{
       this.activeSectionId = item.id;
       document.getElementById('content').insertAdjacentHTML('afterbegin',item.content)
+    }
+  }
+  getSections(){
+    for(let section of this.apiTitles){
+      const promises = []
+      for(let tag of section.tags){
+        const reqObj = {
+          "language": 'RUS',
+          "tag": tag
+        }
+        promises.push(this.staticPageService.getStaticPage(reqObj));
+      }
+      Promise.all(promises).then(resp=>{
+        section.sections = resp.map(el=>{
+          return el.data
+        })
+      });
     }
   }
 
