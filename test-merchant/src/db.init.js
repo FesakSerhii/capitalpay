e3b768061b623655d82fa2f68e99cc48d1bacef8
Mikhail Kramer <mikhailnew84@gmail.com>
2022-01-11 14:48:37 +0200

    test merchant improvement

--- test-merchant/src/db.init.js
+++ test-merchant/src/db.init.js
@@ -6,14 +6,27 @@ function createDBConnection() {
         host: "185.111.106.6",
         database: "merchant_test"
     });
-    pool.query(`
+    createTables(pool);
+    return pool;
+}
+
+async function createTables(pool) {
+    await pool.query(`
         create table if not exists cards (
            id SERIAL primary key not null,
            card_number varchar(50) not null,
            card_token varchar(255) not null,
-           card_id int not null
+           card_id int not null,
+           param varchar(255) null
         )
     `);
-    return pool;
+    await pool.query(`
+        create table if not exists payments (
+            id serial primary key not null,
+            card_id integer not null,
+            sum decimal(10,2) not null,
+            result text
+        )
+    `)
 }
 module.exports = { createDBConnection };
