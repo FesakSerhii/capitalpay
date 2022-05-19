a01c569308a1820e4de79f6be138a6b9173584c5
Serhii <serhiipasalskyi@gmail.com>
2022-05-19 11:27:51 +0300

    Login fix

--- backend/server/src/main/java/kz/capitalpay/server/login/config/AuthenticationFailHandler.java
+++ backend/server/src/main/java/kz/capitalpay/server/login/config/AuthenticationFailHandler.java
@@ -1,26 +1,23 @@
 package kz.capitalpay.server.login.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class AuthenticationFailHandler implements AuthenticationFailureHandler {
-
     @Override
-    public void onAuthenticationFailure(HttpServletRequest request,
-                                        HttpServletResponse response, AuthenticationException exception)
-
-            throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        response.setStatus(HttpStatus.BAD_REQUEST.value());
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-        response.getWriter().write("Authentication failed");
+    public void onAuthenticationFailure(HttpServletRequest httpServletRequest,
+                                        HttpServletResponse httpServletResponse,
+                                        AuthenticationException e) throws IOException, ServletException {
+        httpServletResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+        httpServletResponse.setContentType("application/json");
+        httpServletResponse.setCharacterEncoding("UTF-8");
+        httpServletResponse.getWriter().write("Authentication failed");
     }
 
 }
