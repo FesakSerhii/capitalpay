32350b94e3e762181ca81f91de5a0cf1a484816f
serhiifesak <Serhii.Fesak@saphira.agency>
2022-12-06 06:41:13 +0200

    Форма для курьера

--- frontend/projects/payment-page/src/app/payment-invoicing/payment-invoicing.component.ts
+++ frontend/projects/payment-page/src/app/payment-invoicing/payment-invoicing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-payment-invoicing',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payment-invoicing.component.scss']
 })
 export class PaymentInvoicingComponent implements OnInit {
-
+  formInvoicing = new FormGroup({
+    sum: new FormControl(),
+    consignment: new FormControl(),
+    bin: new FormControl(),
+    email: new FormControl('', [Validators.email]),
+    comment: new FormControl(),
+    bankCards: new FormControl()
+  });
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  bankCardsChange(Number: number){
+    this.formInvoicing.get('bankCards').patchValue(Number);
   }
 
 }
